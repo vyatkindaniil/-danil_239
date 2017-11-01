facf65e740293ac300ab4840587481b7bf1f02f6
daniil <vyatkin.daniiil13@mail.ru>
2017-11-01 13:18:28 +0300

    C:/Program Files (x86)/Git/

--- lesson008/file5.java
+++ lesson008/file5.java
@@ -1,7 +1,28 @@
 package directorii.lesson008;
-
-/**
- * Created by Даниил Алексеевич on 27.10.2017.
- */
+import java.io.*;
+import java.util.*;
+/*Имеется текстовый файл. Добавить в него строку из двенадцати черточек  (------------), разместив ее:
+        а) после пятой строки;
+        б) после последней из строк, в которых нет пробела.
+        Если таких строк нет, то новая строка должна быть добавлена после всех строк имеющегося файла.*/
 public class file5 {
+    public static void main(String[] args) throws Exception {
+        FileReader fileReader = new FileReader("E://111/mane12.txt");
+        Scanner reader = new Scanner(fileReader);
+        ArrayList<String> array = new ArrayList<>();
+        while (reader.hasNextLine()) {
+            array.add(reader.nextLine());
+        }
+        for(int i=0;i<array.size();i++){
+            if (i==5){
+                array.add(5,"----------");
+            }
+        }
+        FileWriter fileWriter1 = new FileWriter("E://111/mane12.txt");
+        for (int i = 0; i < array.size(); i++) {
+            fileWriter1.write(array.get(i));
+            fileWriter1.write("\n");
+        }
+        fileWriter1.close();
+}
 }
